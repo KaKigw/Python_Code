394c284d21f0514cc995cfb80d951caec431c6ec
Zakaria Adda <zaka.adda@gmail.com>
2025-06-25 20:51:37 +0100

    Added a new Activity

--- Intro/Python_Activities/Activities.py
+++ Intro/Python_Activities/Activities.py
@@ -58,6 +58,21 @@ text = "welcome to gomycode"
 count = text.count('o')
 print(f"The letter 'o' occurs {count} times in the string '{text}'.")
 
+#Activity 04: Coin Toss
+""" You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails". Important, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads. There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails. """
+
+import random
+toss = random.randint(0, 1)  # Generate a random number, either 0 or 1
+if toss == 0:
+    print("Heads")
+else:
+    print("Tails")
+    
+
+
+
+
+
 
 
 
